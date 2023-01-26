b5ed316d88df925f62bcd937758b5ccf7cab5923
endless.sine@gmail.com <endless.sine@gmail.com>
2023-01-27 01:14:02 +0200

    gunk

--- arcap-hs/app/Main.hs
+++ arcap-hs/app/Main.hs
@@ -1,16 +1,14 @@
 module Main where
 
-import CapLib 
-import Text.Printf
+import CapLib
 import System.Environment
+import Text.Printf
 
-
-main :: IO () 
-main = do 
-        args <- getArgs
-        let input = args !! 0 
-        let capVal = calcCap (map digitToFloat input)
-        printf "%f uF \n" $ capVal / 1000000
-        printf "%f nF \n" $ capVal / 1000 
-        printf "%f pF \n" capVal 
-
+main :: IO ()
+main = do
+  args <- getArgs
+  let input = head args
+  let capVal = calcCap (map digitToFloat input)
+  printf "%f uF \n" $ capVal / 1000000
+  printf "%f nF \n" $ capVal / 1000
+  printf "%f pF \n" capVal
