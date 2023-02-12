eb2f883080d7dea71d0e70d71f521b6247cd73b9
endless.sine@gmail.com <endless.sine@gmail.com>
2023-02-12 18:37:51 +0200

    fixed formatting

--- arcap-clj/project.clj
+++ arcap-clj/project.clj
@@ -5,6 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :main ^:skip-aot arcap-clj.core
+  :plugins [[lein-cljfmt "0.9.2"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
--- arcap-clj/src/arcap_clj/core.clj
+++ arcap-clj/src/arcap_clj/core.clj
@@ -1,15 +1,15 @@
 (ns arcap-clj.core
   (:gen-class))
 
-
 (defn get-digit
   [string digit]
   (Character/digit (nth string digit) 10))
 
-
 (defn arcap
   [string]
-  (* (+ (* (get-digit string 0) 10) (get-digit string 1)) (Math/pow 10 (get-digit string 2))))
+  (*
+   (+ (* (get-digit string 0) 10) (get-digit string 1))
+   (Math/pow 10 (get-digit string 2))))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -19,5 +19,4 @@
   (printf "%.2f %s \n" (arcap (nth args 0)) "pF")
   (printf "%.3f %s \n" (/ (arcap (nth args 0)) 1000) "nF")
   (printf "%.6f %s \n" (/ (arcap (nth args 0)) 1000000) "uF")
-  (flush)
-  )
+  (flush))
